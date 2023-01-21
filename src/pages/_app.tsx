7c5e15a8dd3ef64162b3dceb643b39ee0226aac0
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2023-01-21 20:39:46 -0300

    chore: add default font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css';
+import '@/styles/global.css';
 import type { AppProps } from 'next/app';
+import { DM_Sans } from '@next/font/google';
+
+const dmSans = DM_Sans({
+  weight: ['400', '500', '700'],
+  style: 'normal',
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={dmSans.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
