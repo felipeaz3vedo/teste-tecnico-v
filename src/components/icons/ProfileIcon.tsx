e5c99677acf5c3f037a66afe5197b56f768637b2
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2023-01-21 21:22:17 -0300

    feat: add profile icon component

---
+++ src/components/icons/ProfileIcon.tsx
@@ -0,0 +1,17 @@
+export function ProfileIcon() {
+  return (
+    <svg
+      className="w-[24px] h-[24px] fill-blue-50 hover:cursor-pointer transition group-hover:fill-purple-700"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <path
+          d="M4 13h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v8c0 .55.45 1 1
+          1Zm0 8h6c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1Zm10
+          0h6c.55 0 1-.45 1-1v-8c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1ZM13 4v4c0
+          .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1Z"
+        />
+      </g>
+    </svg>
+  );
+}
