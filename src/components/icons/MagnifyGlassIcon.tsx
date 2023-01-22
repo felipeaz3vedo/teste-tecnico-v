36af48336eac4eea827d80ad1cea2ec61d064475
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2023-01-21 21:30:16 -0300

    feat: add magnify glass icon component

---
+++ src/components/icons/MagnifyGlassIcon.tsx
@@ -0,0 +1,19 @@
+export function MagnifyGlassIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="11"
+      height="12"
+      fill="none"
+      viewBox="0 0 11 12"
+    >
+      <circle cx="5" cy="5" r="4.3" stroke="#2b3674" strokeWidth="1.4"></circle>
+      <path
+        stroke="#2b3674"
+        strokeLinecap="round"
+        strokeWidth="1.4"
+        d="M10.01 11L8 8.99"
+      ></path>
+    </svg>
+  );
+}
