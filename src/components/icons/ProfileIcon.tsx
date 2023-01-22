20a5d85ac4fc9e4888aced68cb06716b4e4082c0
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2023-01-22 01:45:42 -0300

    fix: put right profile svg path

--- src/components/icons/ProfileIcon.tsx
+++ src/components/icons/ProfileIcon.tsx
@@ -1,15 +1,20 @@
 export function ProfileIcon() {
   return (
     <svg
-      className="w-[24px] h-[24px] fill-blue-50 hover:cursor-pointer transition group-hover:fill-purple-700"
+      className="
+        w-[24px]
+        h-[24px]
+      fill-blue-50
+        hover:cursor-pointer
+        transition
+      group-hover:fill-purple-700"
       xmlns="http://www.w3.org/2000/svg"
     >
       <g>
         <path
-          d="M4 13h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v8c0 .55.45 1 1
-          1Zm0 8h6c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1Zm10
-          0h6c.55 0 1-.45 1-1v-8c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1ZM13 4v4c0
-          .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1Z"
+          d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4
+          4 4Zm0 2c-2.67 0-8 1.34-8 4v1c0 .55.45 1 1 1h14c.55 0 1-.45
+          1-1v-1c0-2.66-5.33-4-8-4Z"
         />
       </g>
     </svg>
